932817a38e3d9c3ff3886e7b47ac80060e5e4522
yatishsingla <61521076+yatishsingla@users.noreply.github.com>
2023-01-17 14:39:19 -0500

    Update streamlit_app.py
    emojis added

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,8 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
-streamlit.text('Avocado Toadt')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toadt')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
